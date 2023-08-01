87f54932eeb56f4144df7a30f4bee2e403eb6c26
Bryan Biedenkapp <gatekeep@jmp.cx>
2023-08-01 09:52:04 -0400

    initial application stub;

---
+++ dvmissi/ConfigurationObject.cs
@@ -0,0 +1,154 @@
+﻿/**
+* Digital Voice Modem - ISSI
+* AGPLv3 Open Source. Use is subject to license terms.
+* DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+*
+* @package DVM / ISSI
+*
+*/
+/*
+*   Copyright (C) 2022 by Bryan Biedenkapp N2PLL
+*
+*   This program is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU Affero General Public License as published by
+*   the Free Software Foundation, either version 3 of the License, or
+*   (at your option) any later version.
+*
+*   This program is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU Affero General Public License for more details.
+*/
+
+using System;
+using System.Collections.Generic;
+
+using dvmissi.FNE;
+
+namespace dvmissi
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ConfigLogObject
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public int DisplayLevel = 1;
+        /// <summary>
+        /// 
+        /// </summary>
+        public int FileLevel = 1;
+        /// <summary>
+        /// 
+        /// </summary>
+        public string FilePath = ".";
+        /// <summary>
+        /// 
+        /// </summary>
+        public string FileRoot = "dvmissi";
+    } // public class ConfigLogObject
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ConfigurationObject
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public ConfigLogObject Log = new ConfigLogObject();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public int PingTime = 5;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool RawPacketTrace = false;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Name = "ISSI";
+        /// <summary>
+        /// 
+        /// </summary>
+        public uint PeerId;
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Address;
+        /// <summary>
+        /// 
+        /// </summary>
+        public int Port;
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Passphrase;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string IssiAddress;
+        /// <summary>
+        /// 
+        /// </summary>
+        public int IssiPort;
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public int SourceId;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public int DestinationId;
+
+        /*
+        ** Methods
+        */
+
+        /// <summary>
+        /// Helper to convert the <see cref="ConfigPeerObject"/> to a <see cref="PeerDetails"/> object.
+        /// </summary>
+        /// <param name="peer"></param>
+        /// <returns></returns>
+        public static PeerDetails ConvertToDetails(ConfigurationObject peer)
+        {
+            PeerDetails details = new PeerDetails();
+
+            // identity
+            details.Identity = peer.Name;
+            details.RxFrequency = 0;
+            details.TxFrequency = 0;
+
+            // system info
+            details.Latitude = 0.0d;
+            details.Longitude = 0.0d;
+            details.Height = 1;
+            details.Location = "Digital Network";
+
+            // channel data
+            details.TxPower = 0;
+            details.TxOffsetMhz = 0.0f;
+            details.ChBandwidthKhz = 0.0f;
+            details.ChannelID = 0;
+            details.ChannelNo = 0;
+
+            // RCON
+            details.Password = "ABCD123";
+            details.Port = 9990;
+
+            details.Software = AssemblyVersion._VERSION;
+
+            return details;
+        }
+    } // public class ConfigurationObject
+} // namespace dvmissi
---
+++ dvmissi/FneSystemBase.P25.cs
@@ -0,0 +1,130 @@
+﻿/**
+* Digital Voice Modem - ISSI
+* AGPLv3 Open Source. Use is subject to license terms.
+* DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+*
+* @package DVM / ISSI
+*
+*/
+/*
+*   Copyright (C) 2023 by Bryan Biedenkapp N2PLL
+*
+*   This program is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU Affero General Public License as published by
+*   the Free Software Foundation, either version 3 of the License, or
+*   (at your option) any later version.
+*
+*   This program is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU Affero General Public License for more details.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+using Serilog;
+
+using dvmissi.FNE;
+using dvmissi.FNE.P25;
+
+namespace dvmissi
+{
+    /// <summary>
+    /// Implements a FNE system base.
+    /// </summary>
+    public abstract partial class FneSystemBase
+    {
+        /*
+        ** Methods
+        */
+
+        /// <summary>
+        /// Callback used to validate incoming P25 data.
+        /// </summary>
+        /// <param name="peerId">Peer ID</param>
+        /// <param name="srcId">Source Address</param>
+        /// <param name="dstId">Destination Address</param>
+        /// <param name="callType">Call Type (Group or Private)</param>
+        /// <param name="duid">P25 DUID</param>
+        /// <param name="frameType">Frame Type</param>
+        /// <param name="streamId">Stream ID</param>
+        /// <returns>True, if data stream is valid, otherwise false.</returns>
+        protected virtual bool P25DataValidate(uint peerId, uint srcId, uint dstId, CallType callType, P25DUID duid, FrameType frameType, uint streamId)
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// Event handler used to pre-process incoming P25 data.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected virtual void P25DataPreprocess(object sender, P25DataReceivedEvent e)
+        {
+            return;
+        }
+
+        /// <summary>
+        /// Event handler used to process incoming P25 data.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected virtual void P25DataReceived(object sender, P25DataReceivedEvent e)
+        {
+            DateTime pktTime = DateTime.Now;
+
+            if (e.DUID == P25DUID.HDU || e.DUID == P25DUID.TSDU || e.DUID == P25DUID.PDU)
+                return;
+
+            byte len = e.Data[23];
+            byte[] data = new byte[len];
+            for (int i = 24; i < len; i++)
+                data[i - 24] = e.Data[i];
+
+            if (e.CallType == CallType.GROUP)
+            {
+                if (e.SrcId == 0)
+                {
+                    Log.Logger.Warning($"({SystemName}) P25D: Received call from SRC_ID {e.SrcId}? Dropping call e.Data.");
+                    return;
+                }
+
+                // ensure destination ID matches
+                if (e.DstId != Program.Configuration.DestinationId)
+                    return;
+
+                // is this a new call stream?
+                if (e.StreamId != status[P25_FIXED_SLOT].RxStreamId && ((e.DUID != P25DUID.TDU) && (e.DUID != P25DUID.TDULC)))
+                {
+                    callInProgress = true;
+                    status[P25_FIXED_SLOT].RxStart = pktTime;
+                    Log.Logger.Information($"({SystemName}) P25D: Traffic *CALL START     * PEER {e.PeerId} SRC_ID {e.SrcId} TGID {e.DstId} [STREAM ID {e.StreamId}]");
+                }
+
+                if (((e.DUID == P25DUID.TDU) || (e.DUID == P25DUID.TDULC)) && (status[P25_FIXED_SLOT].RxType != FrameType.TERMINATOR))
+                {
+                    callInProgress = false;
+                    TimeSpan callDuration = pktTime - status[P25_FIXED_SLOT].RxStart;
+                    Log.Logger.Information($"({SystemName}) P25D: Traffic *CALL END       * PEER {e.PeerId} SRC_ID {e.SrcId} TGID {e.DstId} DUR {callDuration} [STREAM ID {e.StreamId}]");
+                }
+
+
+                // TODO TODO TODO
+
+                status[P25_FIXED_SLOT].RxRFS = e.SrcId;
+                status[P25_FIXED_SLOT].RxType = e.FrameType;
+                status[P25_FIXED_SLOT].RxTGId = e.DstId;
+                status[P25_FIXED_SLOT].RxTime = pktTime;
+                status[P25_FIXED_SLOT].RxStreamId = e.StreamId;
+            }
+            else
+                Log.Logger.Warning($"({SystemName}) P25D: ISSI does not support private calls.");
+
+            return;
+        }
+    } // public abstract partial class FneSystemBase
+} // namespace dvmissi
